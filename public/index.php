165ac59e17d73a72fde4d725ee5c4b56d08de31a
Andador <slayer.han1997@gmail.com>
2020-04-01 23:54:48 +0300

    fix localhost bug

--- public/index.php
+++ public/index.php
@@ -11,11 +11,11 @@ $template = new Template(__DIR__.'/../templates/'.$lang.'/');
 
 $app->route('POST', '/en', function() {
   $_SESSION['lang'] = 'en';
-  header("Location: http://test/");
+  header("Location: /");
 });
 $app->route('POST', '/ru', function() {
   $_SESSION['lang'] = 'ru';
-  header("Location: http://test/");
+  header("Location: /");
 });
 
 $app->route('GET', '/', function () use($template) {
@@ -36,29 +36,29 @@ $app->route('POST', '/sign_in', function () {
   $user = $_POST;
   if ($user['login'] === '') {
     $_SESSION['errorLogin'] = $errors->get('empty');
-    header("Location: http://test/");
+    header("Location: /");
     return;
   }
   $userData = $repo->find($user);
   if (!$userData) {
     $_SESSION['errorLogin'] = $errors->get('login');
-    header("Location: http://test/");
+    header("Location: /");
     return;
   } 
   if (password_verify($user['password'], $userData['password'])) {
     $_SESSION['user'] = $user;
     $_SESSION['data'] = $repo->getUser($user['login']);
-    header("Location: http://test/");
+    header("Location: /");
   } else {
     $_SESSION['errorPassword'] = $errors->get('password');
-    header("Location: http://test/");
+    header("Location: /");
   }
 });
 
 $app->route('POST', '/sign_out', function () use ($template) {
   $_SESSION = [];
   session_destroy();
-  header("Location: http://test/");
+  header("Location: /");
 });
 
 $app->route('GET', '/users/new', function () use ($template) {
@@ -80,7 +80,7 @@ $app->route('POST', '/users', function () use ($template) {
     foreach ($errors as $key => $value) {
       $_SESSION[$key] = $value;
     }
-    header("Location: http://test/users/new");
+    header("Location: /users/new");
     return;
   }
   $repo = new Repository();
@@ -93,7 +93,7 @@ $app->route('POST', '/users', function () use ($template) {
     //the user exists
     $errors = new App\Errors($_SESSION['lang']);
     $_SESSION['alreadyTaken'] = $errors->get('alreadyTaken');
-    header("Location: http://test/users/new");
+    header("Location: /users/new");
     return;
   }
 });
